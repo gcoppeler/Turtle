13d52d6d39fa6958ceea67638e446dc8bde45b75
Gary Coppeler <coppeler@gmail.com>
2018-03-20 13:12:31 -0400

    Move square code into function definition

--- mypolygon.py
+++ mypolygon.py
@@ -1,7 +1,10 @@
 import turtle
 bob = turtle.Turtle()
-print(bob)
-for i in range(4):
-    bob.fd(100)
-    bob.lt(90)
+
+def square(t):
+    for i in range(4):
+        t.fd(100)
+        t.lt(90)
+
+square(bob)
 turtle.mainloop()
