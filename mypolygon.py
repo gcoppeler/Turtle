95098bcee4d4ea97bcb22ef6b0c109a4211a7e9b
Gary Coppeler <coppeler@gmail.com>
2018-03-20 13:13:26 -0400

    Add length parameter to square function

--- mypolygon.py
+++ mypolygon.py
@@ -1,10 +1,10 @@
 import turtle
 bob = turtle.Turtle()
 
-def square(t):
+def square(t, length):
     for i in range(4):
-        t.fd(100)
+        t.fd(length)
         t.lt(90)
 
-square(bob)
+square(bob, 100)
 turtle.mainloop()
