9dd4e74f6a345e4c6fbffe45fbc4531996bd9796
Gary Coppeler <coppeler@gmail.com>
2018-03-20 13:15:08 -0400

    Change square function to more generic polygon code

--- mypolygon.py
+++ mypolygon.py
@@ -1,10 +1,11 @@
 import turtle
 bob = turtle.Turtle()
 
-def square(t, length):
-    for i in range(4):
+def polygon(t, n, length):
+    angle = 360 / n
+    for i in range(n):
         t.fd(length)
-        t.lt(90)
+        t.lt(angle)
 
-square(bob, 100)
+polygon(bob, 7, 70)
 turtle.mainloop()
